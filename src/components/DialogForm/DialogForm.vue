cfeaf683cb114d1dd033af31dd0293c392b07c6f
moling <365024424@qq.com>
2018-08-07 19:43:44 +0800

    添加DialogForm组件

---
+++ src/components/DialogForm/DialogForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="DialogForm">
+    <el-dialog
+      title="提示"
+      :visible.sync="visible"
+      :before-close="handleClose"
+      @closed="handleClosed">
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="handleClose">取 消</el-button>
+        <el-button type="primary" @click="handleClose">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DialogForm',
+  data () {
+    return {
+      visible: false
+    }
+  },
+  methods: {
+    handleClose () {
+      this.visible = false
+    },
+    handleClosed () {
+      this.$emit('closed')
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
