72d602f7c1a579bbcfd7780754378b8e1d39203f
moling <365024424@qq.com>
2018-08-14 09:33:40 +0800

    实现动态表单组件

--- src/components/DialogForm/DialogForm.vue
+++ src/components/DialogForm/DialogForm.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="DialogForm">
-    <el-dialog
-      title="提示"
-      :visible.sync="visible"
-      :before-close="handleClose"
-      @closed="handleClosed">
-      <span>这是一段信息</span>
+    <el-dialog title="提示" :visible.sync="visible" :before-close="close" @closed="handleClosed">
+      <el-form :model="form" :rules="rules" label-width="100px" ref="form">
+        <el-form-item v-for="item in formItems" :key="item.value" :label="item.label" :prop="item.value">
+          <el-input v-model="form[item.value]"></el-input>
+        </el-form-item>
+      </el-form>
       <span slot="footer" class="dialog-footer">
-        <el-button @click="handleClose">取 消</el-button>
-        <el-button type="primary" @click="handleClose">确 定</el-button>
+        <el-button @click="reset">重 置</el-button>
+        <el-button type="primary" @click="submit">提 交</el-button>
       </span>
     </el-dialog>
   </div>
@@ -17,18 +17,53 @@
 <script>
 export default {
   name: 'DialogForm',
+  props: {
+    formItems: {
+      type: Array,
+      required: true
+    },
+    formData: {
+      type: Object,
+      default () {
+        return {}
+      }
+    },
+    rules: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  },
   data () {
     return {
-      visible: false
+      visible: false,
+      form: {}
     }
   },
   methods: {
-    handleClose () {
+    reset () {
+
+    },
+    submit () {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          this.$emit('submit', JSON.parse(JSON.stringify(this.form)), this.close)
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    close () {
       this.visible = false
     },
     handleClosed () {
       this.$emit('closed')
     }
+  },
+  mounted () {
+    this.form = JSON.parse(JSON.stringify(this.formData))
   }
 }
 </script>
