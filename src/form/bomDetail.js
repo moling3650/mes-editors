b2798e9776dc549bb89ee00c1c528cbb7868c613
moling <365024424@qq.com>
2018-08-21 09:26:47 +0800

    新增BOM明细表单

---
+++ src/form/bomDetail.js
@@ -0,0 +1,84 @@
+export default function getBomDetailForm (form = null, type = 'add') {
+  return Promise.resolve({
+    title: `${type === 'add' ? '新建' : '编辑'}BOM明细表单`,
+    formItems: [
+      {
+        value: 'bom_code',
+        label: 'BOM编号',
+        component: 'el-input',
+        disabled: true
+      },
+      {
+        value: 'mat_type',
+        label: '物料类型',
+        component: 'ex-select',
+        options: [{value: 0, label: '成品'}, {value: 1, label: '物料'}],
+        disabled: type === 'edit'
+      },
+      {
+        value: 'mat_code',
+        label: '物料编号',
+        component: 'el-input',
+        disabled: type === 'edit'
+      },
+      {
+        value: 'qty',
+        label: '耗料数量',
+        component: 'el-input-number'
+      },
+      {
+        value: 'base_qty',
+        label: '耗料基数',
+        component: 'el-input-number'
+      },
+      {
+        value: 'wastage',
+        label: '损耗率',
+        component: 'el-input-number'
+      },
+      {
+        value: 'enable_Substitute',
+        label: '能否替代',
+        component: 'el-switch',
+        activeValue: 1,
+        inactiveValue: 0
+      },
+      {
+        value: 'enable_beyond',
+        label: '能否超越',
+        component: 'el-switch',
+        activeValue: 1,
+        inactiveValue: 0
+      },
+      {
+        value: 'be_ctrl',
+        label: '是否可控',
+        component: 'el-switch',
+        activeValue: 1,
+        inactiveValue: 0
+      }
+    ],
+    formData: Object.assign({
+      bom_code: '',
+      mat_type: '',
+      mat_code: '',
+      qty: 1,
+      base_qty: 1,
+      wastage: 1,
+      enable_Substitute: 1,
+      enable_beyond: 1,
+      be_ctrl: 1
+    }, form),
+    rules: {
+      bom_code: [{ required: true, trigger: 'blur' }],
+      mat_type: [{ required: true, trigger: 'blur' }],
+      mat_code: [{ required: true, trigger: 'blur' }],
+      qty: [],
+      base_qty: [],
+      wastage: [],
+      enable_Substitute: [],
+      enable_beyond: [],
+      be_ctrl: []
+    }
+  })
+}
