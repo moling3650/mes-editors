134557eb380f85f5bebbd26ce2c36fb82eb8b631
moling <365024424@qq.com>
2018-08-31 11:25:47 +0800

    把替代料卡片抽离成组件

---
+++ src/components/Cards/SubstituteCard.vue
@@ -0,0 +1,111 @@
+<template>
+  <el-card class="h250">
+    <div slot="header" class="card-header clearfix">
+      <span class="card-header">替代料</span>
+      <el-button :disabled="disabled" class="fl-r p3-0" icon="el-icon-plus" type="text" @click="addSubstitute">添加替代料</el-button>
+    </div>
+    <el-table :data="substitutes" stripe header-cell-class-name="thcell" size="mini" class="w100p">
+      <el-table-column prop="Substitute_mat_code" label="替代料编号"/>
+      <el-table-column prop="Substitute_mat_name" label="替代料名称"/>
+      <el-table-column fixed="right" label="操作" width="80" align="center">
+        <template slot-scope="scope">
+          <el-button-group>
+            <el-button @click.stop="editSubstitute(scope.row)" type="primary" icon="el-icon-edit" circle size="mini"></el-button>
+            <el-button @click.stop="deleteSubstitute(scope.row)" type="danger" icon="el-icon-delete" circle size="mini"></el-button>
+          </el-button-group>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-card>
+</template>
+
+<script>
+import apis from '@/apis'
+import getSubstituteForm from '@/form/substitute'
+
+export default {
+  name: 'SubstituteCard',
+  props: {
+    detail: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    disabled () {
+      return !this.detail.enable_Substitute
+    }
+  },
+  data () {
+    return {
+      substitutes: []
+    }
+  },
+  watch: {
+    'detail.mat_code' (value, oldValue) {
+      this.substitutes = []
+      if (this.detail.enable_Substitute) {
+        this.fetchSubstitutes()
+      }
+    }
+  },
+  methods: {
+    // 操作替代料
+    fetchSubstitutes () {
+      apis.fetchSubstituteMaterial(this.detail.bom_code, this.detail.mat_code).then(data => {
+        this.substitutes = data
+      })
+    },
+
+    addSubstitute () {
+      const formData = {
+        bom_code: this.detail.bom_code,
+        mat_code: this.detail.mat_code
+      }
+      const optionsApi = this.detail.mat_type ? 'fetchMaterialOptions' : 'fetchProductOptions'
+      apis[optionsApi]()
+        .then(options => getSubstituteForm(formData, 'add', options))
+        .then(form => this.$showForm(form).$on('submit', (substitute, close) => {
+          apis.addSubstitute(substitute).then(substitute => {
+            this.substitutes.push(substitute)
+            this.$message.success('添加成功!')
+            close()
+          })
+        }))
+    },
+
+    editSubstitute (row) {
+      const optionsApi = this.detail.mat_type ? 'fetchMaterialOptions' : 'fetchProductOptions'
+      apis[optionsApi]()
+        .then(options => getSubstituteForm(row, 'edit', options))
+        .then(form => this.$showForm(form).$on('submit', (substitute, close) => {
+          apis.updateSubstitute(substitute).then(substitute => {
+            const index = this.substitutes.findIndex(s => s.id === substitute.id)
+            ~index && this.substitutes.splice(index, 1, substitute)
+            this.$message.success('修改成功!')
+            close()
+          })
+        }))
+    },
+
+    deleteSubstitute (substitute) {
+      this.$confirm('此操作将永久删除该替代料, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(_ => {
+        apis.deleteSubstitute(substitute).then(_ => {
+          const index = this.substitutes.findIndex(s => s.id === substitute.id)
+          ~index && this.substitutes.splice(index, 1)
+          this.$message.success('删除成功!')
+        })
+      }).catch(_ => {
+        this.$message.info('已取消删除')
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/pages/BomEditor.vue
+++ src/pages/BomEditor.vue
@@ -51,25 +51,8 @@
 
       <el-col :span="8">
         <BomDetailCard :detail="detail"></BomDetailCard>
+        <SubstituteCard :detail="detail"></SubstituteCard>
 
-        <el-card class="h250">
-          <div slot="header" class="card-header clearfix">
-            <span class="card-header">替代料</span>
-            <el-button :disabled="!detail.enable_Substitute" class="fl-r p3-0" icon="el-icon-plus" type="text" @click="addSubstitute">添加替代料</el-button>
-          </div>
-          <el-table :data="substitutes" stripe header-cell-class-name="thcell" size="mini" class="w100p">
-            <el-table-column prop="Substitute_mat_code" label="替代料编号"/>
-            <el-table-column prop="Substitute_mat_name" label="替代料名称"/>
-            <el-table-column fixed="right" label="操作" width="80" align="center">
-              <template slot-scope="scope">
-                <el-button-group>
-                  <el-button @click.stop="editSubstitute(scope.row)" type="primary" icon="el-icon-edit" circle size="mini"></el-button>
-                  <el-button @click.stop="deleteSubstitute(scope.row)" type="danger" icon="el-icon-delete" circle size="mini"></el-button>
-                </el-button-group>
-              </template>
-            </el-table-column>
-          </el-table>
-        </el-card>
       </el-col>
     </el-row>
 
@@ -81,15 +64,16 @@ import apis from '@/apis'
 import getBomForm from '@/form/bom'
 import getBomDetailForm from '@/form/bomDetail'
 import BomDetailCard from '@/components/Cards/BomDetailCard'
+import SubstituteCard from '@/components/Cards/SubstituteCard'
 
 export default {
   name: 'BomEditor',
   components: {
     BomDetailCard,
+    SubstituteCard
   },
   data () {
     return {
-      showSubstitute: false,
       bomCode: '',
       versionCode: '',
       product: [],
@@ -97,7 +81,6 @@ export default {
       bomList: [],
       bomDetail: [],
       detail: {},
-      substitutes: [],
       props: {
         label: 'mat_name',
         isLeaf (data, node) {
@@ -114,53 +97,6 @@ export default {
     }
   },
   methods: {
-    // 操作替代料
-    addSubstitute () {
-      const formData = {
-        bom_code: this.detail.bom_code,
-        mat_code: this.detail.mat_code
-      }
-      const optionsApi = this.detail.mat_type ? 'fetchMaterialOptions' : 'fetchProductOptions'
-      apis[optionsApi]()
-        .then(options => getSubstituteForm(formData, 'add', options))
-        .then(form => this.$showForm(form).$on('submit', (substitute, close) => {
-          apis.addSubstitute(substitute).then(substitute => {
-            this.substitutes.push(substitute)
-            this.$message.success('添加成功!')
-            close()
-          })
-        }))
-    },
-
-    editSubstitute (substitute) {
-      const optionsApi = this.detail.mat_type ? 'fetchMaterialOptions' : 'fetchProductOptions'
-      apis[optionsApi]()
-        .then(options => getSubstituteForm(substitute, 'edit', options))
-        .then(form => this.$showForm(form).$on('submit', (substitute, close) => {
-          apis.updateSubstitute(form).then(substitute => {
-            const index = this.substitutes.findIndex(s => s.id === substitute.id)
-            ~index && this.substitutes.splice(index, 1, substitute)
-            this.$message.success('修改成功!')
-            close()
-          })
-        }))
-    },
-
-    deleteSubstitute (substitute) {
-      this.$confirm('此操作将永久删除该替代料, 是否继续?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(_ => {
-        apis.deleteSubstitute(substitute).then(_ => {
-          const index = this.substitutes.findIndex(s => s.id === substitute.id)
-          ~index && this.substitutes.splice(index, 1)
-          this.$message.success('删除成功!')
-        })
-      }).catch(_ => {
-        this.$message.info('已取消删除')
-      })
-    },
 
     editBomDetail (node, bomDetail) {
       const optionsApi = bomDetail.mat_type ? 'fetchMaterialOptions' : 'fetchProductOptions'
@@ -265,13 +201,7 @@ export default {
     },
 
     handleNodeClick (node) {
-      this.substitutes = []
       this.detail = node
-      if (node.enable_Substitute) {
-        apis.fetchSubstituteMaterial(node.bom_code, node.mat_code).then(data => {
-          this.substitutes = data
-        })
-      }
     },
 
     loadNode (node, resolve) {
