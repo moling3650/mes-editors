d821d804821d6bf46dc82e2ca6561cb526e59695
moling <365024424@qq.com>
2018-09-03 17:59:27 +0800

    更新ReportEditor组件

--- src/pages/ReportEditor.vue
+++ src/pages/ReportEditor.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="ReportEditor">
-    ReportEditor
+    <el-row :gutter="20" class="row">
+      <el-col :span="8">
+        <ReportButtonGroup :reportCode.sync="reportCode"></ReportButtonGroup>
+      </el-col>
+
+    </el-row>
   </div>
 </template>
 
 <script>
+import ReportButtonGroup from '@/components/ButtonGroup/ReportButtonGroup'
+
 export default {
   name: 'ReportEditor',
+  components: {
+    ReportButtonGroup
+  },
   data () {
     return {
-
+      reportCode: ''
     }
   }
 }
