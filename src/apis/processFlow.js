dae856b1fa7793375cde384282c5c68a2214b9ee
moling <365024424@qq.com>
2018-09-04 17:31:13 +0800

    更新API

--- src/apis/processFlow.js
+++ src/apis/processFlow.js
@@ -6,8 +6,12 @@ export default {
     return execSQL(sql)
   },
 
-  fetchProcessFlowDetailListByFlow (flow) {
-    const sql = 'SELECT * FROM B_Process_Flow_Detail WHERE flow_code = @flow_code ORDER BY idx'
+  fetchProcessListByFlow (flow) {
+    const sql = `
+    SELECT P.process_code, P.process_name FROM B_ProcessList P
+    JOIN B_Process_Flow_Detail D ON D.process_from = P.process_code
+    WHERE D.flow_code = @flow_code
+    ORDER BY D.idx`
     return execSQL(sql, flow)
   }
 }
