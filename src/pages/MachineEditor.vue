0cbab79488a007c1f6378eba05ef2cd0ea5ce38c
moling <365024424@qq.com>
2018-09-10 09:57:47 +0800

    设备管理添加machineType/machineKind组件

--- src/pages/MachineEditor.vue
+++ src/pages/MachineEditor.vue
@@ -1,12 +1,37 @@
 <template>
-  <div id="MachineEditor">MachineEditor</div>
+  <div id="MachineEditor">
+
+    <el-row class="row" :gutter="20">
+      <el-col :span="8">
+        <MachineTypeCard @change="handleMachineTypeChange"></MachineTypeCard>
+      </el-col>
+      <el-col :span="8">
+        <MachineKindCard :typeId="typeId"></MachineKindCard>
+      </el-col>
+      <el-col :span="8"></el-col>
+    </el-row>
+
+  </div>
 </template>
 
 <script>
+import MachineTypeCard from '@/components/Cards/MachineTypeCard'
+import MachineKindCard from '@/components/Cards/MachineKindCard'
+
 export default {
   name: 'MachineEditor',
+  components: {
+    MachineTypeCard,
+    MachineKindCard
+  },
   data () {
     return {
+      typeId: ''
+    }
+  },
+  methods: {
+    handleMachineTypeChange (machineType) {
+      this.typeId = machineType.type_id
     }
   }
 }
