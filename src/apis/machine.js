fca00ab2a29b7d2a600e5e9af0e253a7edd2cc53
moling <365024424@qq.com>
2018-09-20 16:09:29 +0800

    更新api

--- src/apis/machine.js
+++ src/apis/machine.js
@@ -3,7 +3,12 @@ import execSQL from '@/apis/executeSQL'
 export default {
   // 操作BOM
   getMachineListByModel (modelCode) {
-    const sql = 'SELECT * FROM B_Machine WHERE model_code = @model_code'
+    const sql = `
+          SELECT M.*,W.ws_name,D.depart_name FROM B_Machine M inner join S_Department D
+          on M.userdepartment = D.depart_code
+          inner join B_WorkShop W
+          on M.ws_code = W.ws_code
+          WHERE M.model_code = @model_code`
     return execSQL(sql, modelCode)
   },
 
--- src/apis/machineProperty.js
+++ src/apis/machineProperty.js
@@ -3,7 +3,8 @@ import execSQL from '@/apis/executeSQL'
 export default {
   fetchPropertyListByMachineCode (machineCode) {
     const sql = `
-          SELECT P.ppt_id,P.ppt_name, M.machine_code, D.ppt_val FROM B_Machine_Type_Property P
+          SELECT D.id, P.ppt_id,P.ppt_name, P.ppt_type, M.machine_code, D.ppt_val
+          ,D.ppt_max,D.ppt_min,D.ppt_condition,D.[enable] FROM B_Machine_Type_Property P
           JOIN B_Machine_Model MM ON MM.kind_id = P.kind_id
           JOIN B_Machine M ON M.model_code = MM.model_code
           LEFT JOIN B_Machine_Property_Detail D ON D.ppt_id = P.ppt_id
@@ -15,16 +16,24 @@ export default {
     const sql = `
           INSERT INTO B_Machine_Property_Detail
             ( ppt_id
-            , ppt_name
+            , ppt_type
             , machine_code
-            , ppt_val)
+            , ppt_val
+            , ppt_max
+            , ppt_min
+            , ppt_condition
+            , enable)
           VALUES
             ( @ppt_id
-            , @ppt_name
+            , @ppt_type
             , @machine_code
-            , @ppt_val);
+            , @ppt_val
+            , @ppt_max
+            , @ppt_min
+            , @ppt_condition
+            , @enable);
 
-          SELECT TOP (1) * FROM B_Machine_Property_Detail WHERE ppt_id = @ppt_id`
+          SELECT TOP (1) * FROM B_Machine_Property_Detail WHERE ppt_id = @ppt_id and machine_code = @machine_code`
     return execSQL(sql, machinePropertyDetail).then(data => data.pop())
   },
   // 操作B_Machine_Type_Property
@@ -38,10 +47,12 @@ export default {
           INSERT INTO B_Machine_Type_Property
             ( kind_id
             , ppt_name
+            , ppt_type
             , description)
           VALUES
             ( @kind_id
             , @ppt_name
+            , @ppt_type
             , @description);
 
           SELECT TOP (1) * FROM B_Machine_Type_Property WHERE ppt_id = @ppt_id`
@@ -52,11 +63,26 @@ export default {
     const sql = `
           UPDATE B_Machine_Type_Property
             SET ppt_name = @ppt_name
+            , ppt_type = @ppt_type
             , description = @description
           WHERE ppt_id = @ppt_id;
 
           SELECT TOP (1) * FROM B_Machine_Type_Property WHERE ppt_id = @ppt_id`
     return execSQL(sql, machineProperty).then(data => data.pop())
+  },
+
+  updateMachinePropertyDetail (propertyDetail) {
+    const sql = `
+          UPDATE B_Machine_Property_Detail
+            SET ppt_condition = @ppt_condition
+            , ppt_val = @ppt_val
+            , ppt_max = @ppt_max
+            , ppt_min = @ppt_min
+            , enable = @enable
+          WHERE id = @id;
+
+          SELECT TOP (1) * FROM B_Machine_Property_Detail WHERE id = @id`
+    return execSQL(sql, propertyDetail).then(data => data.pop())
   },
 
   deleteMachineProperty (machineProperty) {
