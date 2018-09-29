c464be41cfb1ad9c5fa97518ef9c05172686c5d8
moling <365024424@qq.com>
2018-09-29 17:50:44 +0800

    重构路由，添加侧边栏导航

---
+++ src/pages/Editors.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="Editors">
+    <el-container>
+      <el-aside width="200px" style="margin-top: 35px">
+        <el-menu background-color="#545c64" text-color="#fff" active-text-color="#ffd04b" :router="true" :unique-opened="true">
+          <el-menu-item index="/editors/report">
+            <i class="el-icon-setting"></i>
+            <span slot="title">报表模块</span>
+          </el-menu-item>
+          <el-submenu index="machine">
+            <template slot="title">
+              <i class="el-icon-setting"></i>
+              <span>设备</span>
+            </template>
+            <el-menu-item index="/editors/machine_type">设备类型</el-menu-item>
+            <el-menu-item index="/editors/machine_model">设备型号</el-menu-item>
+            <el-menu-item index="/editors/machine">设备属性</el-menu-item>
+            <el-menu-item index="/editors/control_point">设备管控点</el-menu-item>
+          </el-submenu>
+          <el-submenu index="workTool">
+            <template slot="title">
+              <i class="el-icon-setting"></i>
+              <span>工装</span>
+            </template>
+            <el-menu-item index="/editors/workTool_type">工装类型</el-menu-item>
+            <el-menu-item index="/editors/workTool_model">工装型号</el-menu-item>
+            <el-menu-item index="/editors/mould_type">模具类型</el-menu-item>
+            <el-menu-item index="/editors/mould_model">模具型号</el-menu-item>
+          </el-submenu>
+          <el-submenu index="material">
+            <template slot="title">
+              <i class="el-icon-setting"></i>
+              <span>物料</span>
+            </template>
+            <el-menu-item index="/editors/bom">物料清单</el-menu-item>
+            <el-menu-item index="/editors/formula">物料配方</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <router-view/>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Editors',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,53 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
+const editors = [
+  {
+    path: 'report',
+    component: () => import('@/pages/ReportEditor')
+  },
+  {
+    path: 'bom',
+    component: () => import('@/pages/BomEditor')
+  },
+  {
+    path: 'formula',
+    component: () => import('@/pages/FormulaEditor')
+  },
+  {
+    path: 'control_point',
+    component: () => import('@/pages/ControlPointEditor')
+  },
+  {
+    path: 'machine',
+    component: () => import('@/pages/MachineEditor')
+  },
+  {
+    path: 'machine_type',
+    component: () => import('@/pages/MachineTypeEditor')
+  },
+  {
+    path: 'machine_model',
+    component: () => import('@/pages/MachineModelEditor')
+  },
+  {
+    path: 'workTool_type',
+    component: () => import('@/pages/WorkToolTypeEditor')
+  },
+  {
+    path: 'workTool_model',
+    component: () => import('@/pages/WorkToolModelEditor')
+  },
+  {
+    path: 'mould_type',
+    component: () => import('@/pages/MouldTypeEditor')
+  },
+  {
+    path: 'mould_model',
+    component: () => import('@/pages/MouldModelEditor')
+  }
+]
+
 export default new Router({
   routes: [
     {
@@ -10,49 +57,15 @@ export default new Router({
       component: () => import('@/components/HelloWorld')
     },
     {
-      path: '/editors/report',
-      component: () => import('@/pages/ReportEditor')
+      path: '/editors',
+      component: () => import('@/pages/editors'),
+      children: editors
     },
-    {
-      path: '/editors/bom',
-      component: () => import('@/pages/BomEditor')
-    },
-    {
-      path: '/editors/formula',
-      component: () => import('@/pages/FormulaEditor')
-    },
-    {
-      path: '/editors/control_point',
-      component: () => import('@/pages/ControlPointEditor')
-    },
-    {
-      path: '/editors/machine',
-      component: () => import('@/pages/MachineEditor')
-    },
-    {
-      path: '/editors/machine_type',
-      component: () => import('@/pages/MachineTypeEditor')
-    },
-    {
-      path: '/editors/machine_model',
-      component: () => import('@/pages/MachineModelEditor')
-    },
-    {
-      path: '/editors/workTool_type',
-      component: () => import('@/pages/WorkToolTypeEditor')
-    },
-    {
-      path: '/editors/workTool_model',
-      component: () => import('@/pages/WorkToolModelEditor')
-    },
-    {
-      path: '/editors/mould_type',
-      component: () => import('@/pages/MouldTypeEditor')
-    },
-    {
-      path: '/editors/mould_model',
-      component: () => import('@/pages/MouldModelEditor')
-    }
-
+    ...editors.map(route => {
+      return {
+        path: `/${route.path}`,
+        component: route.component
+      }
+    })
   ]
 })
