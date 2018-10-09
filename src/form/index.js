3042b5d468a9269ddd11c2d86839c091221b24c9
moling <365024424@qq.com>
2018-10-09 12:11:59 +0800

    注册相关表单

--- src/form/index.js
+++ src/form/index.js
@@ -1,10 +1,15 @@
 import MachineType from './machineType'
 import WorkToolType from './workToolType'
 import MouldType from './mouldType'
+import MachinePropertyDetail from './machinePropertyDetail'
+import MouldPropertyDetail from './mouldPropertyDetail'
+import WorkToolPropertyDetail from './workToolPropertyDetail'
 
 export default {
   MachineType,
   WorkToolType,
-  MouldType
-
+  MouldType,
+  MachinePropertyDetail,
+  MouldPropertyDetail,
+  WorkToolPropertyDetail
 }
