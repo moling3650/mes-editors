500e641615c7e5087a1ba562e05805eeb07823fe
moling <365024424@qq.com>
2018-10-11 10:18:06 +0800

    更新MouldEditor页面

--- src/pages/MouldEditor.vue
+++ src/pages/MouldEditor.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="MouldEditor">
-
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <ExTreeCard title="模具管理" model="Mould" height="500"/>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
