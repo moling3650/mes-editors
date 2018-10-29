5a1df1aa0e699eaf57d3402cf0a69cbf55830eb6
moling <365024424@qq.com>
2018-10-29 15:10:08 +0800

    添加工装管理页面组件

---
+++ src/pages/WorkToolEditor.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="WorkToolEditor">
+    <el-row :gutter="20">
+      <el-col :span="8">
+        <ExTreeCard title="工装管理" model="WorkTool" height="500" @change="handleChange" ref="tree"/>
+      </el-col>
+      <el-col :span="8">
+        <ExItemCard :model="model" :item="item" height="500" @deleted="removeItem" @updated="updateItem"/>
+      </el-col>
+      <el-col :span="8">
+        <ExPropsCard :model="model" :item="item" height="500" type="WorkTool"/>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WorkToolEditor',
+  data () {
+    return {
+      model: '',
+      item: {}
+    }
+  },
+  methods: {
+    handleChange (model, data) {
+      this.model = model
+      this.item = data
+    },
+
+    updateItem (item) {
+      this.item = item
+      this.$refs.tree.updateCurrentNode(item)
+    },
+
+    removeItem () {
+      this.model = ''
+      this.item = {}
+      this.$refs.tree.removeCurrentNode()
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
