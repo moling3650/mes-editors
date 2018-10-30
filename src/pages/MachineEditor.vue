f802c60a71ea705f2153f129104627470b4bf2fb
moling <365024424@qq.com>
2018-10-30 09:45:19 +0800

    更新MachineEditor页面组件

--- src/pages/MachineEditor.vue
+++ src/pages/MachineEditor.vue
@@ -1,51 +1,47 @@
 <template>
   <div id="MachineEditor">
-    <el-row class="row" :gutter="20">
+    <el-row :gutter="20">
       <el-col :span="8">
-        <ExSelectButtonGroup model="MachineType" name="设备类型" value="type_id" label="type_name" @change="handleMachineTypeChange"/>
+        <ExTreeCard title="设备管理" model="Machine" height="500" @change="handleChange" ref="tree"/>
+      </el-col>
+      <el-col :span="8">
+        <ExItemCard :model="model" :item="item" height="500" @deleted="removeItem" @updated="updateItem"/>
+      </el-col>
+      <el-col :span="8">
+        <ExPropsCard :model="model" :item="item" height="500" type="Machine"/>
       </el-col>
     </el-row>
-    <el-row class="row" :gutter="20">
-      <el-col :span="8">
-        <MachineKindCard :typeId="typeId" @change="handleMachineKindChange"></MachineKindCard>
-      </el-col>
-      <el-col :span="16">
-        <MachinePropertyCard :kindId="kindId"></MachinePropertyCard>
-      </el-col>
-    </el-row>
-
   </div>
 </template>
 
 <script>
-import MachineTypeCard from '@/components/Cards/MachineTypeCard'
-import MachineKindCard from '@/components/Cards/MachineKindCard'
-import MachinePropertyCard from '@/components/Cards/MachinePropertyCard'
-
 export default {
   name: 'MachineEditor',
-  components: {
-    MachineTypeCard,
-    MachineKindCard,
-    MachinePropertyCard
-  },
   data () {
     return {
-      typeId: '',
-      kindId: ''
+      model: '',
+      item: {}
     }
   },
   methods: {
-    handleMachineTypeChange (machineType) {
-      this.typeId = machineType.type_id
+    handleChange (model, data) {
+      this.model = model
+      this.item = data
     },
 
-    handleMachineKindChange (machineKind) {
-      this.kindId = machineKind.kind_id
+    updateItem (item) {
+      this.item = item
+      this.$refs.tree.updateCurrentNode(item)
+    },
+
+    removeItem () {
+      this.model = ''
+      this.item = {}
+      this.$refs.tree.removeCurrentNode()
     }
   }
 }
 </script>
 
-<style lang="css" scoped>
+<style scoped>
 </style>
