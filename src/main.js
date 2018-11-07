b4a6a53ff1742f1b67c0c4a76a0da3061fb0b107
moling <365024424@qq.com>
2018-11-07 15:35:03 +0800

    Revert "导入v-tree组件"
    This reverts commit a16924280a0fce8e5226ef75041e8536e69da69f.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
-import VTree from 'vue-tree-halower'
 import App from '@/App'
 import router from '@/router'
 import DialogForm from '@/components/DialogForm'
@@ -11,10 +10,8 @@ import ExPropsCard from '@/components/ExPropsCard'
 import ExTreeCard from '@/components/ExTreeCard'
 import ExItemCard from '@/components/ExItemCard'
 import 'element-ui/lib/theme-chalk/index.css'
-import 'vue-tree-halower/dist/halower-tree.min.css' // 你可以自定义树的样式
 
 Vue.use(ElementUI)
-Vue.use(VTree)
 Vue.use(DialogForm)
 Vue.use(ExSelect)
 Vue.use(ExInputNumber)
