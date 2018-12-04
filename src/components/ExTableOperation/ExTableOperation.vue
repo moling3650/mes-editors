ded5f9f5ba7ed9baca0bee7ddce7ad08c77db80b
moling <365024424@qq.com>
2018-12-04 15:03:58 +0800

    Update ExTableOperation.vue

--- src/components/ExTableOperation/ExTableOperation.vue
+++ src/components/ExTableOperation/ExTableOperation.vue
@@ -1,13 +1,17 @@
 <template>
   <el-button-group>
-    <el-button @click.stop.prevent="handleClick('edit')" type="text" icon="el-icon-edit" size="mini"/>
-    <el-button @click.stop.prevent="handleClick('delete')" type="text" icon="el-icon-delete" size="mini"/>
+    <el-button v-for="b in btns" :key="b.type" @click.stop.prevent="handleClick(b.type)" type="text" :icon="`el-icon-${b.icon}`" size="mini"/>
   </el-button-group>
 </template>
 
 <script>
 export default {
   name: 'ExTableOperation',
+  inject: {
+    btns: {
+      default: () => []
+    }
+  },
   props: {
     rowData: {
       type: Object
@@ -21,7 +25,6 @@ export default {
   },
   methods: {
     handleClick (type) {
-      console.log(this)
       this.$emit('on-custom-comp', {type, row: this.rowData, index: this.index})
     }
   }
