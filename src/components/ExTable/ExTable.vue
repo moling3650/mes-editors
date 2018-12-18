6c503c5e7b1fa271554e838614cef06a4725d1f9
moling <365024424@qq.com>
2018-12-18 16:53:11 +0800

    扩展ExTable的持久化和转译的功能

--- src/components/ExTable/ExTable.vue
+++ src/components/ExTable/ExTable.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="ExTable">
-    <div class="button-group"></div>
+    <div class="button-group">
+      <el-button type="primary" size="mini" plain icon="el-icon-plus" @click="add">添加</el-button>
+    </div>
     <v-table
-      style="width:100%"
+      class="v-table"
+      style="width: 100%;"
       is-horizontal-resize
       is-vertical-resize
-      :vertical-resize-offset='60'
+      :vertical-resize-offset="60"
       :multiple-sort="false"
       :min-height="350"
       even-bg-color="#f2f2f2"
@@ -28,17 +31,14 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import Api from '@/utils/Api'
 
 export default {
   name: 'ExTable',
   props: {
     model: {
-      type: String,
-      required: true
-    },
-    cols: {
-      type: Array,
+      type: Object,
       required: true
     }
   },
@@ -57,9 +57,15 @@ export default {
     }
   },
   computed: {
+    ...mapGetters(['opts', 'dictMap']),
     columns () {
       return [
-        ...this.cols,
+        ...this.model.cols.map(col => {
+          if (col.formatter) {
+            col.formatter = this.formatter
+          }
+          return col
+        }),
         {field: 'operation', title: '操作', width: 80, titleAlign: 'center', columnAlign: 'center', componentName: 'ExTableOperation'}
       ]
     },
@@ -68,38 +74,67 @@ export default {
     }
   },
   methods: {
+    formatter (rowData, index, pagingIndex, field) {
+      return this.dictMap[field][rowData[field]]
+    },
+
+    getRowIndex (index) {
+      return (this.pageIndex - 1) * this.pageSize + index
+    },
+
     pageChange (pageIndex) {
       this.pageIndex = pageIndex
     },
+
     pageSizeChange (pageSize) {
       this.pageSize = pageSize
     },
+
     rowClick (rowIndex, rowData) {
       this.currentRow = rowData
     },
+
     rowOperation ({ type, row, index }) {
       this[type](row, index)
     },
-    edit (row, index) {
-      console.log('edit')
+
+    add () {
+      this.model.getForm(null, 'add', this.opts).then(form => this.$showForm(form).$on('submit', (formData, close) => {
+        Api.post(this.model.name, formData).then(newItem => {
+          this.rawData.push(newItem)
+          this.$message.success('添加成功!')
+          close()
+        })
+      }))
     },
+
+    edit (row, index) {
+      this.model.getForm(row, 'edit', this.opts).then(form => this.$showForm(form).$on('submit', (formData, close) => {
+        Api.put(`${this.model.name}/${row[this.model.pk]}`, formData).then(_ => {
+          this.rawData.splice(this.getRowIndex(index), 1, formData)
+          this.$message.success('更新成功!')
+          close()
+        })
+      }))
+    },
+
     delete (row, index) {
       this.$confirm('此操作将永久删除该行的数据, 是否继续?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
       }).then(_ => {
-        // Api.delete(`Boms/${scope.row.bomId}`).then(_ => {
-        this.rawData.splice((this.pageIndex - 1) * this.pageSize + index, 1)
-        this.$message.success('删除成功!')
-        // })
+        Api.delete(`${this.model.name}/${row[this.model.pk]}`).then(_ => {
+          this.rawData.splice(this.getRowIndex(index), 1)
+          this.$message.success('删除成功!')
+        })
       }).catch(_ => {
         this.$message.info('已取消删除')
       })
     }
   },
   created () {
-    Api.get(this.model).then(data => {
+    Api.get(this.model.name).then(data => {
       this.rawData = data
     })
   }
@@ -107,7 +142,7 @@ export default {
 </script>
 
 <style scoped>
-.pagination {
-  margin-top: 10px;
+.v-table {
+  margin: 10px auto;
 }
 </style>
