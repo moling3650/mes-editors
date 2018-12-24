6e374770bfb6b6c9ed24c30f8b6dc51fe34e3b99
moling <365024424@qq.com>
2018-12-24 10:54:18 +0800

    更新WorkGroupClassRest的模型

--- src/models/ProcessFlow/WorkGroupClassRest.js
+++ src/models/ProcessFlow/WorkGroupClassRest.js
@@ -6,8 +6,8 @@ export default {
   // 表格列配置
   cols: [
     {field: 'classId', title: '班别ID', width: 100, titleAlign: 'center', columnAlign: 'center', isResize: true},
-    {field: 'startHour', title: '开始时间（时）', width: 100, titleAlign: 'center', columnAlign: 'center', isResize: true},
-    {field: 'endHour', title: '结束时间（时）', width: 100, titleAlign: 'center', columnAlign: 'center', isResize: true},
+    {field: 'timeStart', title: '开始时间', width: 100, titleAlign: 'center', columnAlign: 'center', isResize: true},
+    {field: 'timeEnd', title: '结束时间', width: 100, titleAlign: 'center', columnAlign: 'center', isResize: true},
     {field: 'description', title: '说明', width: 200, titleAlign: 'center', columnAlign: 'center', isResize: true}
   ],
   // 表单配置
@@ -18,21 +18,21 @@ export default {
       width: '350px',
       // 表单元素
       formItems: [
-        {value: 'startHour', label: '开始（时）', component: 'el-input'},
-        {value: 'endHour', label: '结束（时）', component: 'el-input'},
+        {value: 'timeStart', label: '开始时间', component: 'el-time-select', pickerOptions: {start: '00:00', step: '01:00', end: '23:00'}},
+        {value: 'timeEnd', label: '结束时间', component: 'el-time-select', pickerOptions: {start: '00:00', step: '01:00', end: '23:00'}},
         {value: 'description', label: '说明', component: 'el-input'}
       ],
       // 表单内容
       formData: Object.assign({
         classId: '',
-        startHour: 0,
-        endHour: 0,
+        timeStart: 0,
+        timeEnd: 0,
         description: ''
       }, form),
       // 表单校验细则
       rules: {
-        startHour: [{ required: true, message: '请输入开始时间', trigger: 'blur' }],
-        endHour: [{ required: true, message: '请输入结束时间', trigger: 'blur' }]
+        timeStart: [{ required: true, message: '请输入开始时间', trigger: 'blur' }],
+        timeEnd: [{ required: true, message: '请输入结束时间', trigger: 'blur' }]
       }
     })
   }
